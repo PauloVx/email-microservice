6b80aba139950281f1bfff8cfedd3c556373b9a8
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-01-29 14:39:06 -0300

    Created send mail method inside email service

--- src/main/java/com/ms/email/services/EmailService.java
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -1,9 +1,15 @@
 package com.ms.email.services;
 
-import com.ms.email.dtos.EmailDTO;
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.models.Email;
 import com.ms.email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -11,4 +17,27 @@ public class EmailService {
     @Autowired
     private EmailRepository repository;
 
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public Email sendEmail(Email email) {
+        email.setSendDateEmail(LocalDateTime.now());
+
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setFrom(email.getEmailFrom());
+            message.setTo(email.getEmailTo());
+            message.setSubject(email.getSubject());
+            message.setText(email.getText());
+
+            this.emailSender.send(message);
+        }
+        catch (MailException e) {
+            email.setStatusEmail(StatusEmail.ERROR);
+        }
+        finally {
+            return this.repository.save(email);
+        }
+    }
 }
