6c8b5cbb06b6f2ceb5eb41bc614b83956fae6a62
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-01-29 13:43:57 -0300

    Create EmailService.java

---
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -0,0 +1,14 @@
+package com.ms.email.services;
+
+import com.ms.email.dtos.EmailDTO;
+import com.ms.email.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private EmailRepository repository;
+
+}
