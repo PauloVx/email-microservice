e86e52d6bf072cb86687845da2031eb76f1b7dfb
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-10 17:45:14 -0300

    Change type of emailId to UUID

--- src/main/java/com/ms/email/models/Email.java
+++ src/main/java/com/ms/email/models/Email.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @Entity
@@ -14,7 +15,7 @@ public class Email {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long emailId;
+    private UUID emailId;
     private String ownerRef;
     private String emailFrom;
     private String emailTo;
