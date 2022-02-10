c2f790034c2281a9a60f29daed3ca63757f13c26
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-10 17:52:43 -0300

    Remove useless import.

--- src/main/java/com/ms/email/controllers/EmailController.java
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.io.IOException;
 
 @RestController
 public class EmailController {
